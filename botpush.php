fff24d59c02ee7d40e6f764735feecc395541032
Suphakorn Naenudorn <oot.suphakorn@gmail.com>
2018-07-20 11:12:08 +0700

    Update botpush.php

--- botpush.php
+++ botpush.php
@@ -8,13 +8,19 @@ $access_token = '0B6evNhMG4CGdAWsP2SjJJJb0a91FSERPfQKE6fq8CbRE5k4ZxQz9ebhF6R/9wo
 
 $channelSecret = 'a6f9c0091ab46845a1a6ede91d264c92';
 
-$pushID = '1515198053';
+$pushID = 'U0529947247451731e66d76abe48b1402';
 
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
 
-$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('โปรโมชั่นเดือนกรกฎาคม 2561 ซื้อ 5 ไม่มีของแถมมมมมม');
-$response = $bot->pushMessage($pushID, $textMessageBuilder);
+$placeName = "ที่ตั้งร้าน";
+$placeAddress = "แขวง พลับพลา เขต วังทองหลาง กรุงเทพมหานคร ประเทศไทย";
+$latitude = 13.780401863217657;
+$longitude = 100.61141967773438;
+$replyData = new LocationMessageBuilder($placeName, $placeAddress, $latitude ,$longitude);      
+
+//$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('โปรโมชั่นเดือนกรกฎาคม 2561 ซื้อ 5 ไม่มีของแถมมมมมม');
+$response = $bot->pushMessage($pushID, $replyData);
 
 echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
 
