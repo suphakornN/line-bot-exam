9cfdd267cce78c470da4bc45fbad867b1251f4ac
Suphakorn Naenudorn <oot.suphakorn@gmail.com>
2018-07-20 12:03:43 +0700

    Update botpush.php

--- botpush.php
+++ botpush.php
@@ -37,11 +37,11 @@ use \LINE\LINEBot\MessageBuilder\TemplateBuilder\ImageCarouselTemplateBuilder;
 use \LINE\LINEBot\MessageBuilder\TemplateBuilder\ImageCarouselColumnTemplateBuilder;
 
 
-$access_token = '0B6evNhMG4CGdAWsP2SjJJJb0a91FSERPfQKE6fq8CbRE5k4ZxQz9ebhF6R/9wo4pnmx4VwYeUcUKjGuWZdhG1kjluc/MNm/BTxl4w2fr7D9kYoRkQpJ3W2Rt1bTfukhPFYtzMELJLl+aCB3iAYsiwdB04t89/1O/w1cDnyilFU=';
+$access_token = 'B6evNhMG4CGdAWsP2SjJJJb0a91FSERPfQKE6fq8CbRE5k4ZxQz9ebhF6R/9wo4pnmx4VwYeUcUKjGuWZdhG1kjluc/MNm/BTxl4w2fr7D9kYoRkQpJ3W2Rt1bTfukhPFYtzMELJLl+aCB3iAYsiwdB04t89/1O/w1cDnyilFU=';
 
-$channelSecret = 'a6f9c0091ab46845a1a6ede91d264c92';
+$channelSecret = '6f9c0091ab46845a1a6ede91d264c92';
 
-$pushID = 'U0529947247451731e66d76abe48b1402';
+$pushID = '0529947247451731e66d76abe48b1402';
 
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
